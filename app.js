d5590dfd75f9b6725a7e3bd1d989a0d03ec8d7cd
Al Hasanul Banna <tutul5800@gmail.com>
2023-07-27 11:48:41 +0600

    random pass generator

--- app.js
+++ app.js
@@ -105,3 +105,24 @@ console.log(calculate(15, 3, "/")); // 5
 
 // Problem: 6
 // Create a program that generates a random password of a specified length. The password should include a mix of uppercase letters, lowercase letters, numbers, and special characters.
+function randomPassword(length) {
+  const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  const numbers = "0123456789";
+  const specialCharacters = "!@#$%^&*()-_=+[]{}|;:,.<>?";
+
+  const allCharacters =
+    uppercaseLetters + lowercaseLetters + numbers + specialCharacters;
+
+  let password = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * allCharacters.length);
+    password += allCharacters.charAt(randomIndex);
+  }
+
+  return password;
+}
+
+const passwordLength = 6;
+const generatedPassword = randomPassword(passwordLength);
+console.log(generatedPassword);
